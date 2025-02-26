b806c8789a674a571f37dd026f41df978547c344
Zhukov Sergei <alivesystemsonly@gmail.com>
2025-02-26 20:28:28 +0100

    Added eureka service server

---
+++ eurekaserver/src/test/java/ch/fdsgn/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fdsgn.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
